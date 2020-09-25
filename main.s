af91709093cf4026e814d6c77d24958ed18289fa
Quinn Kleinfelter <qkleinfelter@qkleinfelter.com>
2020-09-25 14:10:23 -0400

    Port F Initialization

--- main.s
+++ main.s
@@ -30,13 +30,68 @@ GPIO_PORTF_PCTL_R       EQU   0x4002552C
 SYSCTL_RCGCGPIO_R       EQU   0x400FE608
 GPIO_PORTF_LOCK_R  	    EQU   0x40025520
 GPIO_PORTF_CR_R         EQU   0x40025524
-SYSCTL_RCGC2_R          EQU   0x400FE108
-unlock_Number           EQU   0x4C4F434B
 
        AREA    |.text|, CODE, READONLY, ALIGN=2
        THUMB
        EXPORT  Start
 Start
+InitPortF
+	; SYSCTL_RCGCGPIO_R = 0x20
+	MOV R0, #0x20
+	LDR R1, =SYSCTL_RCGCGPIO_R
+	STR R0, [R1]
+	
+	LDR R0, [R1] ; Delay before we continue on
+	
+	; Before we write to the CR Register
+	; we need to unlock the port F, using the
+	; constant #0x4C4F434B, however we can't
+	; write this constant directly to using MOV
+	; so we use MOV and MOVT to add it into the register
+	; in 2 parts, note: we must use the MOV command before
+	; MOVT, otherwise the MOV command will overwrite the top
+	; half of the register not unlocking the port
+	; GPIO_PORT_F_LOCK_R = 0x4C4F434B
+	MOV R0, #0x434B
+	MOVT R0, #0x4C4F
+	LDR R1, =GPIO_PORTF_LOCK_R
+	STR R0, [R1]
+
+	; GPIO_PORTF_CR_R = 0x18
+	MOV R0, #0x18
+	LDR R1, =GPIO_PORTF_CR_R
+	STR R0, [R1]
+
+	; GPIO_PORTF_AMSEL_R = 0x00
+	MOV R0, #0x00
+	LDR R1, =GPIO_PORTF_AMSEL_R
+	STR R0, [R1]
+	
+	; GPIO_PORTF_PCTL_R = 0x00
+	MOV R0, #0x00
+	LDR R1, =GPIO_PORTF_PCTL_R
+	STR R0, [R1]
+	
+	; GPIO_PORTF_DIR_R = 0x08
+	MOV R0, #0x08
+	LDR R1, =GPIO_PORTF_DIR_R
+	STR R0, [R1]
+	
+	; GPIO_PORTF_AFSEL_R = 0x00
+	MOV R0, #0x00
+	LDR R1, =GPIO_PORT_AFSEL_DIR_R
+	STR R0, [R1]
+	
+	; GPIO_PORTF_PUR_R = 0x10
+	MOV R0, #0x10
+	LDR R1, =GPIO_PORTF_PUR_R
+	STR R0, [R1]
+	
+	; GPIO_PORTF_DEN_R = 0x18
+	MOV R0, #0x18
+	LDR R1, =GPIO_PORTF_DEN_R
+	STR R0, [R1]
+	
 
 loop  
 
